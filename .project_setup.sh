8095a106262ba0e2266819f5a48ac9f08d8a8101
Dominic <foldvari@mcule.com>
2020-03-29 20:26:52 +0200

    init

---
+++ .project_setup.sh
@@ -0,0 +1,6 @@
+if [ $USER == dominic ]; then
+    export WORKON_HOME=/home/dominic/.virtualenvs
+    # export MCULE_APP_PATH=/home/dominic/.virtualenvs/mcule/app
+    # export MCULE_PROJECT=/home/dominic/.virtualenvs/mcule/app/mcule/mcule
+    source /usr/local/bin/virtualenvwrapper.sh
+fi
